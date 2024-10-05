203ebab01dcc6f62b039b6002db76bdc2b823617
= <solomoncardenbrown@gmail.com>
2024-10-06 04:12:55 +0800

    hash map complete

--- main.c
+++ main.c
@@ -1,21 +1,73 @@
 #define SOL_DEF
 #include "sol.h"
+#include "names.h"
 
 struct thing {
-    int x;
-    struct list l;
+    u64 i;
+    char *s;
+    char c;
 };
+
+def_typed_dict(thing, struct thing)
 
 int main() {
     
-    char *str = "Hello There!";
-    write_file("file.txt", str, strlen(str));
+    allocator_t alloc;
+    create_allocator_arena(NULL, 4096, &alloc);
     
-    u64 size = read_file("file.txt", NULL, 0);
-    char *buf = malloc(size + 1);
-    read_file("file.txt", buf, size);
-    buf[size] = 0;
-    println("size %u, buf %s", size, buf);
+    thing_dict_t dict;
+    create_thing_dict(4, &alloc, &dict);
+    
+    int count = cl_array_len(names);
+    
+    println("\nINSERT");
+    
+    for(int i=0; i < count; ++i) {
+        struct string key = {.data = names[i], .size = strlen(names[i])};
+        struct thing thing = {.i = i, .s = names[i], .c = names[i][0]};
+        if (!thing_dict_insert(&dict, key, &thing)) {
+            println("FAILURE on %u", i);
+            return -1;
+        }
+    }
+    println("    Insert all good");
+    
+    println("\nFIND");
+    
+    for(int i=0; i < count; ++i) {
+        struct string key = {.data = names[i], .size = strlen(names[i])};
+        struct thing_dict_kv *kv = thing_dict_find(&dict, key);
+        if (strcmp(kv->val.s, names[i])) {
+            println("FAILURE on %u", i);
+            return -1;
+        }
+    }
+    println("    Find all good");
+    
+    thing_dict_iter_t iter;
+    thing_dict_get_iter(&dict, &iter);
+    
+    println("\nITER");
+    
+    struct thing_dict_kv *it;
+    u32 counter = 0;
+    dict_for_each(it, &iter, thing_dict_iter_next) {
+        bool found = false;
+        for(int i=0; i < cl_array_len(names); ++i) {
+            if (strcmp(names[i], it->val.s)) {
+                counter++;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            println("FAILURE on %u", counter);
+            return -1;
+        }
+    }
+    println("    Iter all good");
+    
+    destroy_thing_dict(&dict);
     
     return 0;
 }
--- sol.h
+++ sol.h
@@ -48,15 +48,19 @@ typedef uint64_t u64;
 #define memb_to_struct(memb, memb_of, memb_name) \
 ((typeof(memb_of))((u8*)memb - offsetof(typeof(*memb_of), memb_name)))
 
+#define for_bits(pos, count, mask) \
+for(count = 0, pos = (typeof(pos))ctz(mask); \
+count < popcnt(mask); \
+pos = (typeof(pos))ctz(mask & (0xffff << (pos + 1))), ++count)
+
 enum type {
     TYPE_LINEAR,
     TYPE_ARENA,
 };
 
-static inline u64 align(u64 size, u64 alignment) {
-    const u64 alignment_mask = alignment - 1;
-    return (size + alignment_mask) & ~alignment_mask;
-}
+// pp.h
+#define pp_join(a, b) a ## b
+#define pp_struct(name) struct name
 
 // os.h
 extern struct os {
@@ -97,6 +101,7 @@ static inline void rc_init(rc_t *rc)
 
 static inline void rc_inc(rc_t *rc)
 {
+    
     rc->count += 1;
 }
 
@@ -174,6 +179,34 @@ log_break; \
 do { if (prec) log_os_error(__VA_ARGS__); } while(0);
 
 #define invalid_default_case log_error("invalid default case")
+
+// math.h
+static inline bool is_pow2(u64 x)
+{
+    // NOTE(SollyCB): I know that zero is not a power of two, but this function
+    // acts on integers, so any code using it is doing so for alignment and offset purposes,
+    // and zero being valid is therefore useful.
+    return (x & (x - 1)) == 0;
+}
+
+static inline u64 mod_pow2(u64 l, u64 r)
+{
+    assert(is_pow2(r));
+    return l & (r - 1);
+}
+
+static inline u64 next_pow2(u64 x) // TODO(SollyCB): There must be a better implementation...
+{
+    if (x == 0)
+        return 1;
+    return is_pow2(x) ? x : (u64)1 << clz(x);
+}
+
+static inline u64 align(u64 size, u64 alignment) {
+    assert(is_pow2(alignment));
+    u64 alignment_mask = alignment - 1;
+    return (size + alignment_mask) & ~alignment_mask;
+}
 
 // list.h
 typedef struct list list_t;
@@ -310,7 +343,7 @@ struct allocator {
 
 #define alloc_align(size) align(size, 16)
 
-#define def_create_allocator(name, type) void name(void *buf, u64 size, type ## _t *alloc)
+#define def_create_allocator(name, type) int name(void *buf, u64 size, type ## _t *alloc)
 def_create_allocator(create_linear, linear);
 def_create_allocator(create_arena, arena);
 
@@ -341,10 +374,11 @@ def_destroy_allocator(destroy_arena, arena);
 static inline def_create_allocator(create_allocator, allocator)
 {
     switch(alloc->type) {
-        case TYPE_LINEAR: create_linear(buf, size, &alloc->linear); break;
-        case TYPE_ARENA: create_arena(buf, size, &alloc->arena); break;
+        case TYPE_LINEAR: return create_linear(buf, size, &alloc->linear);
+        case TYPE_ARENA: return create_arena(buf, size, &alloc->arena);
         default: invalid_default_case;
     }
+    return -1;
 }
 
 #define create_allocator_linear(buf, size, alloc) \
@@ -421,39 +455,60 @@ struct string {
 typedef struct dict dict_t;
 typedef struct dict_iter dict_iter_t;
 
-#define def_get_dict_key(name) u64 name(struct string key)
+#define def_create_dict(name) int name(u64 size, u64 stride, allocator_t *alloc, dict_t *dict)
 #define def_dict_insert(name) void* dict_insert(dict_t *dict, struct string key, void *val, u64 stride)
 #define def_dict_find(name) void* dict_find(dict_t *dict, struct string key, u64 stride)
 #define def_dict_remove(name) bool dict_remove(dict_t *dict, struct string key, u64 stride)
 #define def_dict_get_iter(name) void dict_get_iter(dict_t *dict, dict_iter_t *iter)
 #define def_dict_iter_next(name) void* dict_iter_next(dict_iter_t *iter, u64 stride)
+#define def_destroy_dict(name) void destroy_dict(dict_t *dict, u64 stride)
 
-def_get_dict_key(get_dict_key);
+def_create_dict(create_dict);
 def_dict_insert(dict_insert);
 def_dict_find(dict_find);
 def_dict_remove(dict_remove);
 def_dict_get_iter(dict_get_iter);
 def_dict_iter_next(dict_iter_next);
+def_destroy_dict(destroy_dict);
 
-#define def_typed_dict(abbrev, val) \
-struct abbrev ## _dict_kv { \
+#define def_get_dict_key(name) u64 name(struct string key)
+def_get_dict_key(get_dict_key);
+
+#define dict_for_each(kv, dict_iter, func) \
+for(kv = func(dict_iter); kv; kv = func(dict_iter))
+
+#define def_dict_kv(name, type) \
+struct name { \
 u64 key; \
-typeof(*val) val; \
-}; \
-typedef struct abbrev ## _dict { \
+type val; \
+};
+
+#define def_dict(name, kv_name) \
+typedef struct name { \
 u32 cap; \
 u32 rem; \
-typeof(val) data; \
+kv_name *data; \
 allocator_t *alloc; \
-} abbrev ## dict_t; \
-\
-typedef struct abbrev ## _dict_iter { \
-abbrev ## _dict_t *dict; \
+} name ## _t;
+
+#define def_dict_iter(name, dict_name) \
+typedef struct name { \
+dict_name *dict; \
 u32 pos; \
-} abbrev ## _dict_iter_t; \
+} name ## _t;
+
+#define def_typed_dict(abbrev, value) \
+def_dict_kv(abbrev ## _dict_kv, typeof(value)) \
+def_dict(abbrev ## _dict, pp_struct(abbrev ## _dict_kv)) \
+def_dict_iter(abbrev ## _dict_iter, abbrev ## _dict_t) \
 \
+static inline void \
+create_ ## abbrev ## _dict(u64 size, allocator_t *alloc, abbrev ## _dict_t *dict) \
+{ \
+create_dict(size, sizeof(*dict->data), alloc, (dict_t*)dict); \
+} \
 static inline struct abbrev ## _dict_kv* \
-abbrev ## _dict_insert(abbrev ## _dict_t *dict, struct string key, typeof(val) val) \
+abbrev ## _dict_insert(abbrev ## _dict_t *dict, struct string key, typeof(value) *val) \
 { \
 return (struct abbrev ## _dict_kv*)dict_insert((dict_t*)dict, key, val, sizeof(*dict->data)); \
 } \
@@ -464,10 +519,9 @@ abbrev ## _dict_find(abbrev ## _dict_t *dict, struct string key) \
 return (struct abbrev ## _dict_kv*)dict_find((dict_t*)dict, key, sizeof(*dict->data)); \
 } \
 \
-static inline struct abbrev ## _dict_kv* \
-abbrev ## _dict_remove(abbrev ## _dict_t *dict, struct string key) \
+static inline bool abbrev ## _dict_remove(abbrev ## _dict_t *dict, struct string key) \
 { \
-return (struct abbrev ## _dict_kv*)dict_remove((dict_t*)dict, key, sizeof(*dict->data)); \
+return dict_remove((dict_t*)dict, key, sizeof(*dict->data)); \
 } \
 \
 static inline void abbrev ## _dict_get_iter(abbrev ## _dict_t *dict, abbrev ## _dict_iter_t *iter) \
@@ -475,10 +529,14 @@ static inline void abbrev ## _dict_get_iter(abbrev ## _dict_t *dict, abbrev ## _
 dict_get_iter((dict_t*)dict, (dict_iter_t*)iter); \
 } \
 \
-static inline abbrev ## _dict_kv* \
+static inline struct abbrev ## _dict_kv* \
 abbrev ## _dict_iter_next(abbrev ## _dict_iter_t *iter) \
 { \
 return (struct abbrev ## _dict_kv*)dict_iter_next((dict_iter_t*)iter, sizeof(*iter->dict->data)); \
+} \
+static inline void destroy_ ## abbrev ## _dict(abbrev ## _dict_t *dict) \
+{ \
+destroy_dict((dict_t*)dict, sizeof(*dict->data)); \
 }
 
 #ifdef SOL_DEF
@@ -806,7 +864,7 @@ struct arena_header* arena_find_block(arena_t *alloc, void *p)
     struct arena_header *block;
     u32 count = alloc->block_count;
     list_for(block, &alloc->block_list, list, count) {
-        if ((u8*)p > block->linear.data &&
+        if ((u8*)p >= block->linear.data &&
             (u8*)p < block->linear.data + block->linear.size)
             break;
     }
@@ -836,12 +894,16 @@ def_create_allocator(create_linear, linear)
 {
     size = alloc_align(size);
     
-    if (!buf)
+    if (!buf) {
         buf = os_allocate(size);
+        if (!buf)
+            return -1;
+    }
     
     alloc->data = buf;
     alloc->size = size;
     alloc->used = 0;
+    return 0;
 }
 
 def_allocate(linear_allocate, linear)
@@ -898,6 +960,7 @@ def_create_allocator(create_arena, arena)
     alloc->min_block_size = (u32)size;
     alloc->block_count = 0;
     create_list(&alloc->block_list);
+    return 0;
 }
 
 def_allocate(arena_allocate, arena)
@@ -976,23 +1039,16 @@ def_destroy_allocator(destroy_arena, arena)
 }
 
 // dict.c
-typedef struct dict {
-    u32 cap;
-    u32 rem;
-    u8 *data;
-    allocator_t *alloc;
-} dict_t;
+#define DICT_VALUE_PLACHOLDER u64
+def_dict(dict, u8)
+def_dict_kv(dict_kv, DICT_VALUE_PLACHOLDER)
+def_dict_iter(dict_iter, dict_t)
 
 typedef struct dict_probe {
     dict_t *dict;
     u32 pos;
     u32 stride;
 } dict_probe_t;
-
-typedef struct dict_iter {
-    dict_t *dict;
-    u32 pos;
-} dict_iter_t;
 
 enum {
     DICT_EMPTY = 0x0,
@@ -1001,25 +1057,163 @@ enum {
 
 #define DICT_GROUP_SIZE 16
 
-uint64_t rapidhash(const void *key, size_t len);
+#define dict_for_each_internal(dict_iter, kv, func, stride) \
+for(kv = func(dict_iter, stride); kv; kv = func(dict_iter, stride))
 
-static void dict_get_probe(dict_t *dict, dict_probe_t *probe)
+#define dict_probe_loop(idx, probe) \
+for(idx = dict_probe_next(probe); idx != Max_u32; idx = dict_probe_next(probe))
+
+#define def_dict_insert_hash(name) static void* name(dict_t *dict, u64 key, void *val, u64 stride)
+def_dict_insert_hash(dict_insert_hash);
+
+static inline u8 dict_top7(u64 key)
 {
-    probe->dict = dict;
-    probe->pos = 0;
+    return (u8)(key >> 57);
+}
+
+static inline u32 dict_group(dict_t *dict, u64 key)
+{
+    u32 i = (u32)mod_pow2(key, dict->cap);
+    return i - (u32)mod_pow2(i, DICT_GROUP_SIZE);
+}
+
+static inline u32 dict_next_group(u32 pos)
+{
+    return pos + DICT_GROUP_SIZE - (u32)mod_pow2(pos, DICT_GROUP_SIZE);
+}
+
+static inline u32 dict_next_full_slot(dict_t *dict, u32 pos)
+{
+    if (pos >= dict->cap)
+        return Max_u32;
+    __m128i a = _mm_load_si128((__m128i*)(dict->data + dict_group(dict, pos)));
+    u16 mask = (u16)_mm_movemask_epi8(a);
+    mask >>= mod_pow2(pos, DICT_GROUP_SIZE);
+    return mask ? pos + (u32)ctz(mask) : Max_u32;
+}
+
+static inline u32 dict_next_empty_slot(dict_t *dict, u32 pos)
+{
+    __m128i a = _mm_load_si128((__m128i*)(dict->data + dict_group(dict, pos)));
+    u16 mask = ~(u16)_mm_movemask_epi8(a);
+    mask &= 0xffff << mod_pow2(pos, DICT_GROUP_SIZE);
+    return mask ? pos + (u32)ctz(mask) : Max_u32;
+}
+
+static inline u16 dict_match(dict_t *dict, u32 group, u8 bits)
+{
+    log_error_if(mod_pow2(group, DICT_GROUP_SIZE), "Group must be a multiple of 16");
+    __m128i a = _mm_load_si128((__m128i*)(dict->data + group));
+    __m128i b = _mm_set1_epi8(bits);
+    a = _mm_cmpeq_epi8(a, b);
+    return (u16)_mm_movemask_epi8(a);
+}
+
+u64 rapidhash(void *key, u64 len);
+
+static void dict_get_probe(dict_t *dict, u32 start, dict_probe_t *probe)
+{
+    probe->dict = (dict_t*)dict;
+    probe->pos = start;
     probe->stride = 0;
 }
 
 static u32 dict_probe_next(dict_probe_t *probe)
 {
-    u32 ret = probe->pos;
-    probe->stride += 16;
-    
     if (probe->stride >= probe->dict->cap)
         return Max_u32;
     
-    probe->pos = (probe->pos + probe->stride) & (probe->dict->cap - 1);
-    return ret;
+    probe->pos = (u32)mod_pow2(probe->pos + probe->stride, probe->dict->cap);
+    probe->stride += DICT_GROUP_SIZE;
+    return probe->pos;
+}
+
+static int dict_copy(dict_t *new_dict, dict_t *old_dict, u64 stride)
+{
+    dict_iter_t it;
+    dict_get_iter(old_dict, &it);
+    
+    struct dict_kv *kv;
+    dict_for_each_internal(&it, kv, dict_iter_next, stride) {
+        if (!dict_insert_hash(new_dict, kv->key, &kv->val, stride))
+            return -1;
+    }
+    return 0;
+}
+
+static u32 dict_find_hash(dict_t *dict, u64 key, u64 stride)
+{
+    dict_probe_t probe;
+    dict_get_probe(dict, dict_group(dict, key), &probe);
+    
+    u32 gr;
+    dict_probe_loop(gr, &probe) {
+        u16 mask = dict_match(dict, gr, DICT_FULL | dict_top7(key));
+        
+        if (!mask)
+            continue;
+        
+        u32 i,cnt;
+        for_bits(i, cnt, mask) {
+            if (key == *(u64*)(dict->data + dict->cap + stride * (gr + i)))
+                return gr + i;
+        }
+    }
+    return Max_u32;
+}
+
+def_create_dict(create_dict)
+{
+    dict->cap = (u32)align(align(size, 16), next_pow2(size));
+    dict->data = allocate(alloc, dict->cap + dict->cap * stride);
+    
+    if (!dict->data) {
+        log_error("Failed to allocate memory for dict");
+        return -1;
+    }
+    
+    memset(dict->data, 0, dict->cap);
+    dict->rem = dict->cap / 8 * 7;
+    dict->alloc = alloc;
+    return 0;
+}
+
+def_dict_insert_hash(dict_insert_hash)
+{
+    if (dict->rem == 0) {
+        dict_t old_dict = *dict;
+        if (create_dict(dict->cap * 2, stride, dict->alloc, dict) ||
+            dict_copy(dict, &old_dict, stride))
+        {
+            log_error("Failed to initialize new dict on resize");
+            *dict = old_dict;
+            return NULL;
+        }
+        destroy_dict(&old_dict, stride);
+    }
+    
+    dict_probe_t probe;
+    dict_get_probe(dict, dict_group(dict, key), &probe);
+    
+    u32 gr;
+    dict_probe_loop(gr, &probe) {
+        u32 i = dict_next_empty_slot(dict, gr);
+        if (i != Max_u32) {
+            u8 *slot = dict->data + i;
+            *slot = DICT_FULL | dict_top7(key);
+            
+            struct dict_kv *kv = (typeof(kv))(dict->data + dict->cap + stride * i);
+            kv->key = key;
+            
+            void *v = &kv->val;
+            memcpy(v, val, stride - 8);
+            
+            dict->rem -= 1;
+            return kv;
+        }
+    }
+    log_error("Failed to find empty slot");
+    return NULL;
 }
 
 def_get_dict_key(get_dict_key)
@@ -1029,17 +1223,25 @@ def_get_dict_key(get_dict_key)
 
 def_dict_insert(dict_insert)
 {
-    return NULL;
+    return dict_insert_hash(dict, rapidhash(key.data, key.size), val, stride);
 }
 
 def_dict_find(dict_find)
 {
-    return NULL;
+    u32 i = dict_find_hash(dict, rapidhash(key.data, key.size), stride);
+    return i == Max_u32 ? NULL : dict->data + dict->cap + stride * i;
 }
 
 def_dict_remove(dict_remove)
 {
-    return false;
+    u32 i = dict_find_hash(dict, rapidhash(key.data, key.size), stride);
+    
+    if (i == Max_u32)
+        return false;
+    
+    u8 *slot = dict-> data + i;
+    *slot = DICT_EMPTY;
+    return true;
 }
 
 def_dict_get_iter(dict_get_iter)
@@ -1050,27 +1252,25 @@ def_dict_get_iter(dict_get_iter)
 
 def_dict_iter_next(dict_iter_next)
 {
-    u32 ofs = iter->pos & (DICT_GROUP_SIZE - 1);
-    u32 grp = iter->pos - ofs;
-    u16 mask = (u16)_mm_movemask_epi8(*(__m128i*)(iter->dict->data + grp)) & (0xffff << ofs);
-    
-    if (!mask)
-        iter->pos += DICT_GROUP_SIZE - (iter->pos & (DICT_GROUP_SIZE - 1));
-    else
-        goto group_found;
-    
-    for(; iter->pos < iter->dict->cap; iter->pos += DICT_GROUP_SIZE) {
-        mask = (u16)_mm_movemask_epi8(*(__m128i*)(iter->dict->data + iter->pos));
-        if (mask)
-            goto group_found;
+    u32 i;
+    while(1) {
+        if (iter->pos >= iter->dict->cap)
+            return NULL;
         
-        iter->pos += DICT_GROUP_SIZE;
+        i = dict_next_full_slot(iter->dict, iter->pos);
+        if (i != Max_u32)
+            break;
+        
+        iter->pos = dict_next_group(iter->pos);
     }
-    return NULL;
-    
-    group_found:
-    iter->pos += ctz16(mask) + 1;
-    return iter->dict->data + iter->dict->cap + stride * (iter->pos-1);
+    iter->pos = i + 1;
+    return iter->dict->data + iter->dict->cap + stride * i;
+}
+
+def_destroy_dict(destroy_dict)
+{
+    deallocate(dict->alloc, dict->data, dict->cap + dict->cap * stride);
+    memset(dict, 0, sizeof(*dict));
 }
 
 //               ***************** EXTERNAL CODE *******************
@@ -1294,7 +1494,7 @@ RAPIDHASH_INLINE uint64_t rapidhash_withSeed(const void *key, size_t len, uint64
     return rapidhash_internal(key, len, seed, rapid_secret);
 }
 
-uint64_t rapidhash(const void *key, size_t len) RAPIDHASH_NOEXCEPT {
+uint64_t rapidhash(void *key, size_t len) RAPIDHASH_NOEXCEPT {
     return rapidhash_withSeed(key, len, RAPID_SEED);
 }
 
